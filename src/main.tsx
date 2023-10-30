4367a24bc9e607f725d15e3299964ef1c83e1040
vchho <12765624+vchho@users.noreply.github.com>
2023-10-29 21:39:19 -0700

    updated app.tsx and main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,46 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom/client";
+// import App from "./App.tsx";
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import { ChakraProvider } from "@chakra-ui/react";
+
+import { QueryClientProvider, QueryClient } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+
+import {
+  Navigate,
+  RouterProvider,
+  createBrowserRouter,
+} from "react-router-dom";
+import TopStories from "./pages/topstories.tsx";
+import { Navbar } from "./components/navbar.tsx";
+import StoryView from "./components/story-view.tsx";
+
+const queryClient = new QueryClient();
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Navigate to="/top-stories" />,
+  },
+  {
+    path: "/top-stories",
+    element: <TopStories />,
+  },
+  {
+    path: "/top-stories/:id",
+    element: <StoryView />,
+  },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <ChakraProvider>
+      <QueryClientProvider client={queryClient}>
+        <ReactQueryDevtools initialIsOpen={false} />
+        <Navbar />
+        <RouterProvider router={router} />
+      </QueryClientProvider>
+    </ChakraProvider>
   </React.StrictMode>,
-)
+);
