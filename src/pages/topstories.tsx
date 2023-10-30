9453b9db96853c8e4daf23195f3bcc3c9b12a63a
vchho <12765624+vchho@users.noreply.github.com>
2023-10-29 21:40:13 -0700

    created top stories page

---
+++ src/pages/topstories.tsx
@@ -0,0 +1,27 @@
+import Stories from "../components/stories";
+
+import { getTopStories } from "../api/hnService";
+
+import { Container } from "@chakra-ui/react";
+
+import { useQuery } from "@tanstack/react-query";
+
+const TopStories = () => {
+  const { data } = useQuery({
+    queryKey: ["top-stories"],
+    queryFn: getTopStories,
+    staleTime: 5000,
+  });
+
+  return (
+    <>
+      <Container maxW="container.xl">
+        {data
+          ?.slice(0, 10)
+          .map((storyId) => <Stories storyId={storyId} key={storyId} />)}
+      </Container>
+    </>
+  );
+};
+
+export default TopStories;
